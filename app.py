f927e07bfb6de350f17026fa90dc3c3e8586edd9
Anthony-Code6 <69853540+Anthony-Code6@users.noreply.github.com>
2025-05-01 03:14:06 -0500

    no autocomplete html

--- app.py
+++ app.py
@@ -177,6 +177,6 @@ def limpiar_imagenes_no_usadas():
 
 
 if __name__ == '__main__':
-    app.run()
-    #app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0',port=8000, debug=True)
+    #app.run()
 
