5c57e69575ed32a0bc2707e87189aece4bd8e50f
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2025-05-17 08:55:12 -0500

    Organizacion de los archivos carpeta static

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -10,19 +10,19 @@ main_bp = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-@main_bp.route('/importar', methods=['GET', 'POST'])
-@login_required
-def importar():
-    if request.method == 'POST':
-        archivo = request.files['modelo']
-        if archivo:
-            ruta_local = f"/tmp/{archivo.filename}"
-            archivo.save(ruta_local)
-            repo.upload_model(ruta_local, archivo.filename)
-            repo.historial_ins(archivo.filename, os.path.getsize(ruta_local))
-            return redirect('/importar')
-    historial = repo.historial_sellst()
-    return render_template('importar.html', historial=historial)
+# @main_bp.route('/importar', methods=['GET', 'POST'])
+# @login_required
+# def importar():
+#     if request.method == 'POST':
+#         archivo = request.files['modelo']
+#         if archivo:
+#             ruta_local = f"/tmp/{archivo.filename}"
+#             archivo.save(ruta_local)
+#             repo.upload_model(ruta_local, archivo.filename)
+#             repo.historial_ins(archivo.filename, os.path.getsize(ruta_local))
+#             return redirect('/importar')
+#     historial = repo.historial_sellst()
+#     return render_template('importar.html', historial=historial)
 
 @main_bp.route('/revisar')
 def revisar():
