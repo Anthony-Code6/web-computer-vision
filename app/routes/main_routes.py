877b299f826c0d90b78cbdd23cde3f18b24a117c
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2025-06-02 09:17:50 -0500

    delete content

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -8,20 +8,6 @@ main_bp = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-# @main_bp.route('/importar', methods=['GET', 'POST'])
-# @login_required
-# def importar():
-#     if request.method == 'POST':
-#         archivo = request.files['modelo']
-#         if archivo:
-#             ruta_local = f"/tmp/{archivo.filename}"
-#             archivo.save(ruta_local)
-#             repo.upload_model(ruta_local, archivo.filename)
-#             repo.historial_ins(archivo.filename, os.path.getsize(ruta_local))
-#             return redirect('/importar')
-#     historial = repo.historial_sellst()
-#     return render_template('importar.html', historial=historial)
-
 @main_bp.route('/detecciones')
 def detecciones():
     return render_template("detecciones.html")
